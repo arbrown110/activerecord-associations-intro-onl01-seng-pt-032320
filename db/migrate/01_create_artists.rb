7a2d12866829ab8d7cbb83490d680c940522acf0
chic-caller-7520 <arbrown110@gmail.com>
2020-06-05 04:09:40 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,14 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
 
   def change
-     create_table :shows do |t|
+     create_table :genres do |t|
        t.string :name
-       t.string :day
-       t.string :network
-       t.integer :rating
      end
    end
  end
-
-
-end
